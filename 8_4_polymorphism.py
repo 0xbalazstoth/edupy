b64ab8c614845049adec792b4781b38636f18cbf
Tóth Balázs <grandmastar27@gmail.com>
2024-07-05 17:12:54 +0200

    Poly updated

--- 8_4_polymorphism.py
+++ 8_4_polymorphism.py
@@ -5,6 +5,18 @@ class Position:
         pass
 
 
+class Player:
+    def __init__(self, name, position):
+        self.name = name
+        self.position = position
+
+    def show_name(self):
+        return self.name
+
+    def show_position(self):
+        return self.position.show_position()
+
+
 class Goalkeeper(Position):
     def show_position(self):
         return "Goalkeeper"
@@ -25,18 +37,24 @@ class Forward(Position):
         return "Forward"
 
 
-def show_player_position(player):
-    print(player.position())
+def show_player_info(player):
+    print(f"Player: {player.show_name()}, Position: {player.show_position()}")
 
 
-# Creating instances of each class
-goalkeeper = Goalkeeper()
-defender = Defender()
-midfielder = Midfielder()
-forward = Forward()
+# Creating instances of each position class
+goalkeeper_position = Goalkeeper()
+defender_position = Defender()
+midfielder_position = Midfielder()
+forward_position = Forward()
+
+# Creating instances of Player class with different positions
+goalkeeper = Player("Buffon", goalkeeper_position)
+defender = Player("Piqué", defender_position)
+midfielder = Player("Iniesta", midfielder_position)
+forward = Player("Ronaldinho", forward_position)
 
 # Demonstrating polymorphism
-show_player_position(goalkeeper)
-show_player_position(defender)
-show_player_position(midfielder)
-show_player_position(forward)
+show_player_info(goalkeeper)
+show_player_info(defender)
+show_player_info(midfielder)
+show_player_info(forward)
